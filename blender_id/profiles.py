7a06dff294bd853d4a858bacca4a5436f648b58e
Sybren A. Stüvel <sybren@stuvel.eu>
2016-03-07 14:46:28 +0100

    Create profiles.json with the correct permissions.
    It used to be a world-readable file, it's now user-readable only.

--- blender_id/profiles.py
+++ blender_id/profiles.py
@@ -6,44 +6,52 @@ profiles_path = bpy.utils.user_resource('CONFIG', "blender_id", create=True)
 profiles_file = os.path.join(profiles_path, 'profiles.json')
 
 
-def get_profiles_data():
-    """Returns the profiles.json content from a blender_id folder in the
-    Blender config directory. If the file does not exist we create one with the
-    basic data structure.
-    """
+def _create_default_file():
+    """Creates the default profile file, returning its contents."""
 
     profiles_default_data = {
         'active_profile': None,
         'profiles': {}
     }
 
-    # if the file does not exist
-    if not os.path.exists(profiles_file):
-        os.makedirs(profiles_path, exist_ok=True)
+    os.makedirs(profiles_path, exist_ok=True)
 
-        # populate the file
+    # Populate the file, ensuring that its permissions are restrictive enough.
+    old_umask = os.umask(0o077)
+    try:
         with open(profiles_file, 'w') as outfile:
             json.dump(profiles_default_data, outfile)
-        return profiles_default_data
+    finally:
+        os.umask(old_umask)
+
+    return profiles_default_data
+
+
+def get_profiles_data():
+    """Returns the profiles.json content from a blender_id folder in the
+    Blender config directory. If the file does not exist we create one with the
+    basic data structure.
+    """
+
+    # if the file does not exist
+    if not os.path.exists(profiles_file):
+        return _create_default_file()
 
     # try parsing the file
-    else:
-        with open(profiles_file, 'r') as f:
-            try:
-                file_data = json.load(f)
-                file_data['active_profile']
-                file_data['profiles']
-                return file_data
-            except (ValueError,  # malformed json data
-                    KeyError):  # it doesn't have the expected content
-                print("(%s) "
-                      "Warning: profiles.json is either empty or malformed. "
-                      "The file will be reset." % __name__)
+    with open(profiles_file, 'r') as f:
+        try:
+            file_data = json.load(f)
+            file_data['active_profile']
+            file_data['profiles']
+            return file_data
+        except (ValueError,  # malformed json data
+                KeyError):  # it doesn't have the expected content
+            print("(%s) "
+                  "Warning: profiles.json is either empty or malformed. "
+                  "The file will be reset." % __name__)
 
-                # overwrite the file
-                with open(profiles_file, 'w') as outfile:
-                    json.dump(profiles_default_data, outfile)
-                return profiles_default_data
+            # overwrite the file
+            return _create_default_file()
 
 
 def get_active_user_id():
